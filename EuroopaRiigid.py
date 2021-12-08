5a120ed507c4cafab153295a5a83d30e09c9be6f
opilane <opilane@HP-CZC6297B89>
2021-12-08 12:31:07 +0200

    1 denj

--- EuroopaRiigid.py
+++ EuroopaRiigid.py
@@ -1,1 +1,12 @@
+capitals=dict()
+key = input("Sisesta country: ")
+item = input("Sisesta capital: ")
+capitals[key] = item
+print(capitals)
 
+data={'Russia': 'Moscow'}
+country=input('Sisesta country: ')
+if country in data:
+    print('Capital is:', data[country])
+else:
+    print('See riigi on puudub')
