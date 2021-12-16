11a25693bc39c00cb9b2f008b303907c3e1c034e
opilane <opilane@HP-CZC6297B89>
2021-12-16 12:30:05 +0200

    gotovo

--- EuroopaRiigid.py
+++ EuroopaRiigid.py
@@ -1,12 +1,22 @@
-capitals=dict()
-key = input("Sisesta country: ")
-item = input("Sisesta capital: ")
-capitals[key] = item
-print(capitals)
-
-data={'Russia': 'Moscow'}
-country=input('Sisesta country: ')
-if country in data:
-    print('Capital is:', data[country])
-else:
-    print('See riigi on puudub')
+from module1 import *
+f=open('riigid.txt' , 'r')
+riigid={}
+riigid1={}
+riig=[]
+linn=[]
+for line in f:
+    k, v=line.strip().split('-')
+    riigid[k.strip()] = v.strip()
+    riigid1[v.strip()] = k.strip()
+    riig.append(k)
+    linn.append(riigid[k.strip()])
+z=''
+while True:
+    z=int(input('1 - Ввод страны и показ его столицы\n2 - Тест'))
+    if z==1:
+        poisk(riigid,riigid1)
+    elif z==2:
+        test(riigid,riig)
+    else:
+        print('Выбери 1 или 2')
+        break
---
+++ module1.py
@@ -0,0 +1,36 @@
+from random import *
+def poisk(riigid:dict,riigid1:dict):
+    a=input('Какой страны столицу вы хотите узнать?')
+    if a not in riigid:
+        b=input('Такой страны нет в словаре. Хотите ввести ее?\n1-Да\n2-Нет')
+        if b==1:
+            c=input('Введите страну: ')
+            d=input('Введите столицу данной страны: ')
+            riigid.update({c:d})
+    else:
+        print(riigid[a])
+    a=input('Какую страну вы хотите узнать по столице?')
+    if a not in riigid1:
+        b=input('Такой столицы нет в словаре. Хотите ввести ее?\n1-Да\n2-Нет')
+        if b==1:
+            c=input('Введите столицу: ')
+            d=input('Введите страну: ')
+            riigid1.update({c:d})
+    else:
+        print(riigid[a])
+
+def test(riigid:dict,riig:list):
+    h=int(input('Сколько вопросов хотите в тесте?'))
+    b=0
+    for i in range(h):
+        n=randint(0,49)
+        a=riig[n]
+        p=riigid[riig[n]]
+        k=input(f'Введите столицу данного гос-ва,{a},{n}')
+        if k!=p:
+            print('Неправильно!')
+        else:
+            print('Правильно!')
+            b+1
+    o=b/h*100
+    print(f'У вас {o} процентов!')
